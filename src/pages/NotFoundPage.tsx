322394cd1c3b9aa2114b20eda7bf3e4df522ce8e
tluoy <tluoy33@gmail.com>
2026-09-02 10:03:23 +0300

    fix: simplify message on NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ function NotFoundPage() {
   return (
     <main>
       <h1>Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page does not exist.</p>
     </main>
   )
 }
